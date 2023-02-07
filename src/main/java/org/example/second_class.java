703d29bce63dff835a46810f0d55ae14c01c88eb
ahmadm520 <ahmadm520@gmail.com>
2023-02-07 13:23:54 -0500

    Initial commit new class

---
+++ src/main/java/org/example/second_class.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class second_class {
+    public static void main(String[] args) {
+        System.out.println(" hasan");
+        System.out.println("Ahmed");
+    }
+}
